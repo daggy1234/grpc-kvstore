fa150f59da650fc9261cbf71ae31cf84ef78e9b0
Arnav Jindal <arnav.jindal7@gmail.com>
2023-06-12 14:10:19 -0400

    working UI

--- kvstore/src/client.rs
+++ kvstore/src/client.rs
@@ -2,42 +2,119 @@ pub mod kvstore {
     tonic::include_proto!("kvstore");
 }
 
+use inquire::{required, Select, Text};
+use kvstore::Item;
 use kvstore::{kvstore_client::KvstoreClient, GetParams};
-use kvstore::{Item};
 use tonic::Request;
 
 use crate::kvstore::AllItemsParams;
 
+#[derive(Debug, Clone, Copy)]
+enum SelectOption {
+    GetItem,
+    PutItem,
+    ListAll,
+    Delete,
+    Exit,
+}
+
+impl std::fmt::Display for SelectOption {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SelectOption::GetItem => write!(f, "Get an Item"),
+            SelectOption::PutItem => write!(f, "Add an Item"),
+            SelectOption::ListAll => write!(f, "List all Item"),
+            SelectOption::Delete => write!(f, "Delete an Item"),
+            SelectOption::Exit => write!(f, "Exit"),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = KvstoreClient::connect("http://[::1]:10000").await?;
-    
+
     // let response = client.get(Request::new(GetParams {
     //     key: "hello".to_string()
     // })).await?;
 
-    let response = client.put(Request::new(Item {
-        key: "hello".to_string(),
-        value: "world".to_string(),
-    })).await?;
-
-    println!("RESPONSE = {:?}", response);
-
-    let responseb = client.put(Request::new(Item {
-        key: "lovely".to_string(),
-        value: "home".to_string(),
-    })).await?;
-
-    println!("RESPONSE = {:?}", responseb);
-
-    let mut all_itms_stream = client.get_all_items(Request::new(AllItemsParams {})).await?.into_inner();
-
-    while let Some(itm) = all_itms_stream.message().await? {
-        print!("Recieved: {:?}\n", itm);
+    loop {
+        let option_vec = vec![
+            SelectOption::GetItem,
+            SelectOption::PutItem,
+            SelectOption::ListAll,
+            SelectOption::Delete,
+            SelectOption::Exit,
+        ];
+        let ans = Select::new("Chose Option from Menu?", option_vec).prompt();
+        match ans {
+            Ok(opt) => match opt {
+                SelectOption::GetItem => {
+                    let key = Text::new("Key of Item:")
+                        .with_validator(required!("This field is required"))
+                        .prompt()?;
+                    let response = client.get(Request::new(GetParams { key })).await.unwrap();
+                    println!("{:?}", response);
+                }
+                SelectOption::PutItem => {
+                    let key = Text::new("Key of Item:")
+                        .with_validator(required!("This field is required"))
+                        .prompt()?;
+                    let value = Text::new("Value of Item:")
+                        .with_validator(required!("This field is required"))
+                        .prompt()?;
+                    let response = client.put(Request::new(Item { key, value })).await.unwrap();
+                    println!("{:?}", response);
+                }
+                SelectOption::ListAll => {
+                    let mut all_itms_stream = client
+                        .get_all_items(Request::new(AllItemsParams {}))
+                        .await?
+                        .into_inner();
+                    while let Some(itm) = all_itms_stream.message().await? {
+                        println!("Recieved: {:?}\n", itm);
+                    }
+                }
+                SelectOption::Delete => {
+                    let key = Text::new("Key of Item:")
+                        .with_validator(required!("This field is required"))
+                        .prompt()?;
+                    let response = client
+                        .delete(Request::new(GetParams { key }))
+                        .await
+                        .unwrap();
+                    println!("{:?}", response);
+                }
+                SelectOption::Exit => {
+                    println!("EXIT");
+                    break;
+                }
+            },
+            Err(e) => {
+                break;
+            }
+        };
     }
 
-     Ok(())
+    // let response = client.put(Request::new(Item {
+    //     key: "hello".to_string(),
+    //     value: "world".to_string(),
+    // })).await?;
+
+    // println!("RESPONSE = {:?}", response);
+
+    // let responseb = client.put(Request::new(Item {
+    //     key: "lovely".to_string(),
+    //     value: "home".to_string(),
+    // })).await?;
+
+    // println!("RESPONSE = {:?}", responseb);
+
+    // let mut all_itms_stream = client.get_all_items(Request::new(AllItemsParams {})).await?.into_inner();
+
+    // while let Some(itm) = all_itms_stream.message().await? {
+    //     print!("Recieved: {:?}\n", itm);
+    // }
+
+    Ok(())
 }
-
-
--- kvstore/src/server.rs
+++ kvstore/src/server.rs
@@ -1,109 +1,106 @@
-
-
 pub mod kvstore {
     tonic::include_proto!("kvstore");
 }
 
-use futures_core::Stream;
 use kvstore::kvstore_server::{Kvstore, KvstoreServer};
-use kvstore::{AllItems, AllItemsParams, GetParams, Item,  Success};
-use tonic::codegen::http::request;
+use kvstore::{AllItemsParams, GetParams, Item, Success};
 use std::collections::HashMap;
-use std::hash::Hash;
-use std::pin::Pin;
-use std::sync::{Arc};
-use tokio::sync::Mutex;
-use tonic::transport::Server;
-use std::unimplemented;
+
+use std::sync::Arc;
 use tokio::sync::mpsc;
+use tokio::sync::Mutex;
 use tokio_stream::wrappers::ReceiverStream;
+use tonic::transport::Server;
 use tonic::{Request, Response, Status};
-
-
 
 #[derive(Debug)]
 struct KvstoreService {
-    data: Arc<Mutex<HashMap<String, String>>>
+    data: Arc<Mutex<HashMap<String, String>>>,
 }
 
 #[tonic::async_trait]
 impl Kvstore for KvstoreService {
-
-
-
-
     async fn get(&self, request: Request<GetParams>) -> Result<Response<Item>, Status> {
         let hm = &self.data.lock().await;
         match hm.get(&request.get_ref().key) {
             Some(s) => {
                 let prod = Item {
                     key: request.get_ref().key.to_string(),
-                    value: s.to_string()
+                    value: s.to_string(),
                 };
-                return Ok(Response::new(prod))
-            },
+                return Ok(Response::new(prod));
+            }
             None => {
                 let prod = Item {
                     key: "".to_string(),
-                    value: "".to_string()
+                    value: "".to_string(),
                 };
-                return Ok(Response::new(prod))
+                return Ok(Response::new(prod));
             }
         }
-    // } else {
-    //     let prod = Item {
-    //                 key: "".to_string(),
-    //                 value: "".to_string()
-    //             };
-    //             return Ok(Response::new(prod))
-    // }
-        
+        // } else {
+        //     let prod = Item {
+        //                 key: "".to_string(),
+        //                 value: "".to_string()
+        //             };
+        //             return Ok(Response::new(prod))
+        // }
     }
 
-async fn put(&self, request: Request<Item>) -> Result<Response<Item>, Status> {
-    let itms = request.get_ref();
-    let key = itms.key.to_string();
-    let val = itms.value.to_string();
-    let mut hm = self.data.lock().await;
-    hm.insert(key, val);
-    Ok(Response::new(Item { key: "".to_string(), value: "".to_string() }))
+    async fn put(&self, request: Request<Item>) -> Result<Response<Item>, Status> {
+        let itms = request.get_ref();
+        let key = itms.key.to_string();
+        let val = itms.value.to_string();
+        let mut hm = self.data.lock().await;
+        hm.insert(key, val);
+        Ok(Response::new(Item {
+            key: "".to_string(),
+            value: "".to_string(),
+        }))
+    }
 
-}
+    async fn delete(&self, request: Request<GetParams>) -> Result<Response<Success>, Status> {
+        let itms = request.get_ref();
+        let key = itms.key.to_string();
+        let mut hm = self.data.lock().await;
+        let out = hm.remove(&key);
+        Ok(Response::new(match out {
+            Some(_v) => Success { success: true },
+            None => Success { success: false },
+        }))
+    }
 
+    type GetAllItemsStream = ReceiverStream<Result<Item, Status>>;
 
-async fn delete(&self, _request: Request<GetParams>) -> Result<Response<Success>, Status> {
-    unimplemented!()
-}
+    async fn get_all_items(
+        &self,
+        _request: Request<AllItemsParams>,
+    ) -> Result<Response<Self::GetAllItemsStream>, Status> {
+        let (tx, rx) = mpsc::channel(4);
+        let data = self.data.clone();
 
-type GetAllItemsStream = ReceiverStream<Result<Item,Status>>;
+        tokio::spawn(async move {
+            let hm = data.lock().await.clone();
+            for itm in hm.into_iter() {
+                tx.send(Ok(Item {
+                    key: itm.0,
+                    value: itm.1,
+                }))
+                .await
+                .unwrap()
+            }
+        });
 
-async fn get_all_items(&self, _request: Request<AllItemsParams>) -> Result<Response<Self::GetAllItemsStream>, Status> {
-    let (mut tx, rx) = mpsc::channel(4);
-    let data = self.data.clone();
-    
-    tokio::spawn(async move {
-        let hm = data.lock().await.clone();
-        for itm in hm.into_iter() {
-            tx.send(Ok(Item {
-                key: itm.0,
-                value: itm.1,
-            })).await.unwrap()
-        }
-    });
-
-    Ok(Response::new(ReceiverStream::new(rx)))
-
+        Ok(Response::new(ReceiverStream::new(rx)))
     }
 }
-
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:10000".parse().unwrap();
 
     let route_guide = KvstoreService {
-        data: Arc::new(Mutex::new(HashMap::new()))
+        data: Arc::new(Mutex::new(HashMap::new())),
     };
 
     let svc = KvstoreServer::new(route_guide);
